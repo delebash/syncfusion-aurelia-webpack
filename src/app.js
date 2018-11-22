0ce25708f550255ae23bcc319e5faf7e59cb173a
Daniel Elebash <danelebash@hotmail.com>
2018-11-22 04:01:55 -0500

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import {PLATFORM} from 'aurelia-pal';
+
+export class App {
+  configureRouter(config, router) {
+    config.title = 'Aurelia';
+    config.map([
+      { route: ['', 'welcome'], name: 'welcome',      moduleId: PLATFORM.moduleName('./welcome'),      nav: true, title: 'Welcome' },
+      { route: 'users',         name: 'users',        moduleId: PLATFORM.moduleName('./users'),        nav: true, title: 'Github Users' },
+      { route: 'child-router',  name: 'child-router', moduleId: PLATFORM.moduleName('./child-router'), nav: true, title: 'Child Router' },
+      { route: 'grid',          name: 'grid',       moduleId: PLATFORM.moduleName('./samples/grid/grid'), nav: true, title: 'Grid' }
+    ]);
+
+    this.router = router;
+  }
+}
